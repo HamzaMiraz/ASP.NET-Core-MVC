addd7d8cc688eaaa6c554101cdac2b0a01289523
HamzaMiraz <amirhamza11170@gmil.com>
2025-08-02 10:48:32 +0600

    first commit

---
+++ UniEF/Data/UniEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniEF.Models;
+
+namespace UniEF.Data
+{
+    public class UniEFContext : DbContext
+    {
+        public UniEFContext (DbContextOptions<UniEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UniEF.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ UniEF/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniEF.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Display(Name="Student Name")]
+        [Required]
+        public string Name { get; set; }
+        public string Department { get; set; }
+        
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+}
